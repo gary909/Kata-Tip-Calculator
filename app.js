3a6c2770d2e9db2ed654f878eb80409b35d72962
gary909 <garywhite61@hotmail.com>
2023-01-05 13:21:21 +0100

    code working

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function calculateTip(amount, rating) {
-
+    rating = rating.toLowerCase();
+    if (rating === "excellent") {
+        return Math.ceil((amount / 100) * 20);
+    } else if (rating === "great") {
+        return Math.ceil((amount / 100) * 15);
+    } else if (rating === "good") {
+        return Math.ceil((amount / 100) * 10);
+    } else if (rating === "poor") {
+        return Math.ceil((amount / 100) * 5);
+    } else if (rating === "terrible") {
+        return Math.ceil((amount / 100) * 0);
+    } else {
+        return "Rating not recognised"
+    }
 }
 
 console.log(calculateTip(20, "Excellent")); // 4
